2cc758bb123264300617a04765a0f9ece7881680
rfuzzo <r.fuzzo@gmail.com>
2023-08-10 15:53:25 +0200

    fix ci

--- Immersive Travel/create_mod.ps1
+++ Immersive Travel/create_mod.ps1
@@ -1,15 +1,7 @@
-Copy-Item -PATH MWSE -Destination "_out/00 Core/MWSE" -Recurse
-Copy-Item -PATH *.toml -Destination "_out/00 Core"
+Copy-Item -PATH MWSE -Destination "_out/MWSE" -Recurse
 
-mkdir "_out/01 Maps and Compass Integration"
-Copy-Item -PATH immersive_maps_compass.esp -Destination "_out/01 Maps and Compass Integration"
-
-mkdir "_out/02 Gridmaps Integration"
-Copy-Item -PATH immersive_maps_gridmap.esp -Destination "_out/02 Gridmaps Integration"
-
-mkdir "_out/03 Mels Map Pack Integration"
-Copy-Item -PATH immersive_maps_mel.esp -Destination "_out/03 Mels Map Pack Integration"
-
-Remove-Item -Path ImmersiveMaps.zip
-Compress-Archive -PATH _out/* -DestinationPath "ImmersiveMaps.zip"
+if (Test-Path ImmersiveTravel.zip) {
+	Remove-Item -Path ImmersiveTravel.zip
+}
+Compress-Archive -PATH _out/* -DestinationPath "ImmersiveTravel.zip"
 Remove-Item -Path _out -Recurse
