7ef07e3e2746eeb52f43945d0201fe4b7c7328bc
rfuzzo <r.fuzzo@gmail.com>
2023-08-22 16:37:48 +0200

    fix mod

--- Immersive Travel/create_mod.ps1
+++ Immersive Travel/create_mod.ps1
@@ -1,4 +1,5 @@
 Copy-Item -PATH MWSE -Destination "_out/MWSE" -Recurse
+Copy-Item -PATH Meshes -Destination "_out/Meshes" -Recurse
 Copy-Item -PATH immersivetravel.esp -Destination "_out"
 
 if (Test-Path ImmersiveTravel.zip) {
