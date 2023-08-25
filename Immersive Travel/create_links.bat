e96ac8985da7a9ebad87c1fc20ea489829478732
rfuzzo <r.fuzzo@gmail.com>
2023-08-25 15:34:45 +0200

    bain

--- Immersive Travel/create_links.bat
+++ Immersive Travel/create_links.bat
@@ -4,7 +4,6 @@ set "location=E:\gog\Morrowind2\Data Files"
 set "drive=%~d0"
 
 mklink /J "%location%\MWSE\mods\rfuzzo\ImmersiveTravel" "%drive%MWSE\mods\rfuzzo\ImmersiveTravel"
-mklink "%location%\immersive_travel-metadata.toml" "%drive%immersive_travel-metadata.toml"
 
 
 pause
