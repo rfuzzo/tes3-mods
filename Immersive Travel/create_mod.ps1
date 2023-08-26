bfddfcdb182846d7ef63d3b4d6bc9e920f997f9f
rfuzzo <r.fuzzo@gmail.com>
2023-08-26 20:42:55 +0200

    Update create_mod.ps1

--- Immersive Travel/create_mod.ps1
+++ Immersive Travel/create_mod.ps1
@@ -1,9 +1,5 @@
-Copy-Item -PATH MWSE -Destination "_out/MWSE" -Recurse
-Copy-Item -PATH Meshes -Destination "_out/Meshes" -Recurse
-Copy-Item -PATH immersivetravel.esp -Destination "_out"
-
 if (Test-Path ImmersiveTravel.zip) {
 	Remove-Item -Path ImmersiveTravel.zip
 }
-Compress-Archive -PATH _out/* -DestinationPath "ImmersiveTravel.zip"
-Remove-Item -Path _out -Recurse
+
+Compress-Archive -Path "00 Core", "01 BCOM","02 Gnisis Docks", "03_TR"  -DestinationPath "ImmersiveTravel.zip"
