a93c40e63caaac1fa5d3769a5c87ca385a8fa28b
rfuzzo <r.fuzzo@gmail.com>
2023-08-27 18:05:08 +0200

    Update create_mod.ps1

--- Immersive Travel/create_mod.ps1
+++ Immersive Travel/create_mod.ps1
@@ -2,4 +2,4 @@ if (Test-Path ImmersiveTravel.zip) {
 	Remove-Item -Path ImmersiveTravel.zip
 }
 
-Compress-Archive -Path "00 Core", "01 BCOM","02 Gnisis Docks"  -DestinationPath "ImmersiveTravel.zip"
+Compress-Archive -Path "00 Core", "01 BCOM", "02 Gnisis Docks", "99 Editor"  -DestinationPath "ImmersiveTravel.zip"
