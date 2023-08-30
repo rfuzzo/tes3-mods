09e013b86f8a879c93804fb50cc40e5f74b0142d
rfuzzo <r.fuzzo@gmail.com>
2023-08-30 14:12:24 +0200

    cleanup TR

--- Immersive Travel/10 World Addon/MWSE/mods/rfuzzo/ImmersiveTravelAddon/main.lua
+++ Immersive Travel/10 World Addon/MWSE/mods/rfuzzo/ImmersiveTravelAddon/main.lua
@@ -21,3 +21,43 @@ local log = logger.new {
 
 -- /////////////////////////////////////////////////////////////////////////////////////////
 -- ////////////// CLASSES
+
+-- /////////////////////////////////////////////////////////////////////////////////////////
+-- ////////////// EVENTS
+--- @param e initializedEventData
+local function initializedCallback(e)
+    debug.log("initializedCallback")
+
+    local services = common.loadServices()
+    if not services then return end
+
+    debug.log(" main")
+    local map = {}
+
+    for key, service in pairs(services) do
+        debug.log(service.class)
+
+        common.loadRoutes(service)
+        local destinations = service.routes
+        if destinations then
+
+            for _i, start in ipairs(table.keys(destinations)) do
+                for _j, destination in ipairs(destinations[start]) do
+                    local spline =
+                        common.loadSpline(start, destination, service)
+                    for _, pos in ipairs(spline) do
+                        local cx = math.floor(pos.x / 8192)
+                        local cy = math.floor(pos.y / 8192)
+
+                        debug.log(cx .. " " .. cy)
+
+                    end
+                end
+            end
+        end
+
+    end
+
+    debug.log("end initializedCallback")
+end
+event.register(tes3.event.initialized, initializedCallback)
