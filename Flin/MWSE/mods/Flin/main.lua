b1b8e6eedcede45d22d71e71b29bf3cf08f2766f
Moritz Baron <r.fuzzo@gmail.com>
2024-05-24 19:51:06 +0200

    add metadata

--- Flin/MWSE/mods/Flin/main.lua
+++ Flin/MWSE/mods/Flin/main.lua
@@ -24,22 +24,22 @@ local function loadedCallback(e)
 end
 event.register(tes3.event.loaded, loadedCallback)
 
---- @param e keyDownEventData
-local function keyDownCallback(e)
-    if e.isAltDown then
-        if e.keyCode == tes3.scanCode["o"] then
-            local t = lib.getLookedAtReference()
-            if t then
-                local refBelow = lib.FindRefBelow(t)
-                if refBelow then
-                    local position = lib.findPlayerPosition(refBelow)
-                    lib.DEBUG_ShowMarkerAt(position)
-                end
-            end
-        end
-    end
-end
-event.register(tes3.event.keyDown, keyDownCallback)
+-- --- @param e keyDownEventData
+-- local function keyDownCallback(e)
+--     if e.isAltDown then
+--         if e.keyCode == tes3.scanCode["o"] then
+--             local t = lib.getLookedAtReference()
+--             if t then
+--                 local refBelow = lib.FindRefBelow(t)
+--                 if refBelow then
+--                     local position = lib.findPlayerPosition(refBelow)
+--                     lib.DEBUG_ShowMarkerAt(position)
+--                 end
+--             end
+--         end
+--     end
+-- end
+-- event.register(tes3.event.keyDown, keyDownCallback)
 
 
 -- /////////////////////////////////////////////////////////////////////////////////////////
