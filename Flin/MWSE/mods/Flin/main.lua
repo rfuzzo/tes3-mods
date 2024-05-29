5ff59cbd2f14b3e48e9871d0492e3d2020795955
Moritz Baron <r.fuzzo@gmail.com>
2024-05-29 18:41:39 +0200

    Update main.lua

--- Flin/MWSE/mods/Flin/main.lua
+++ Flin/MWSE/mods/Flin/main.lua
@@ -1,4 +1,5 @@
 local lib = require("Flin.lib")
+local log = lib.log
 
 -- --- @param e keyDownEventData
 -- local function keyDownCallback(e)
@@ -16,6 +17,29 @@ local lib = require("Flin.lib")
 --     end
 -- end
 -- event.register(tes3.event.keyDown, keyDownCallback)
+
+local GAME_TOPIC = "game of Flin"
+local DECK_TOPIC = "deck of Flin cards"
+local RULES_TOPIC = "rules of Flin"
+
+--- @param e loadedEventData
+local function loadedCallback(e)
+    local result = tes3.addTopic({
+        topic = GAME_TOPIC
+    })
+    log:debug("addTopic %s: %s", GAME_TOPIC, result)
+
+    local result2 = tes3.addTopic({
+        topic = DECK_TOPIC
+    })
+    log:debug("addTopic %s: %s", DECK_TOPIC, result2)
+
+    local result3 = tes3.addTopic({
+        topic = RULES_TOPIC
+    })
+    log:debug("addTopic %s: %s", RULES_TOPIC, result3)
+end
+event.register(tes3.event.loaded, loadedCallback)
 
 
 -- /////////////////////////////////////////////////////////////////////////////////////////
